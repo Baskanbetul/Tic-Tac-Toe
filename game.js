42f192f7ec8ae85597cdb618e615f11d443ceb1c
Baskanbetul <btlbskn@hotmail.com>
2022-04-22 20:57:00 -0600

    Add taking turn functionility

--- game.js
+++ game.js
@@ -1,22 +1,30 @@
 // var Player = require("./player");
 
-var player1 = new Player(id, token);
-var player2 = new Player(id, token);
 
 class Game{
   constructor() {
-    this.player1 = []
-    this.player2 = []
-    this.turn = player1
+    this.player1 = new Player("Skull", 💀 );
+    this.player2 = new Player("Alien", 👽);
+    this.turn = this.player1.token;
+    this.gameContainer = {box1: '', box2: '', box3: '',
+                          box4: '', box5: '', box6: '',
+                          box7: '', box8: '', box9: '' }
+    // this.moveCounter = 0;
+    this.winner = ''
   }
-   takeTurn() {
-      if (this.turn === player1) {
-        this.turn = player2
-    } else if (this.turn === player2) {
-      this.turn = player1
+   takeTurn(emoji) {
+      if (emoji === this.player1.token) {
+        this.turn = this.player2.token
+    } else if (emoji === this.player2.token) {
+      this.turn = this.player1.token
     }
    }
-   showWinner() {
+   determineWinner() {
+     if (this.gameContainer.box1 === this.gameContainer.box2 && this.gameContainer.box2 === this.gameContainer.box3 && this.gameContainer.box1 === this.gameContainer.box3) {
+       console.log(this.turn);
+     }
+     // } else if () {}
+     // if (1===2 && 2===3 && 3==)
      // if click 1 2 3 horizantally you ll // WARNING:
      // '' 4 5 6
      // 7 8 9
--- player.js
+++ player.js
@@ -6,6 +6,7 @@ class Player{
   constructor(id,token) {
     this.id = id
     this.token = token
+    this.isWinner = true
     this.wins = 0
 
   increaseWins() {
